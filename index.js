a7a5d05d908b79d91e3e361c0ae829f4fb9aefe8
Jonatan <jonatansan1@hotmail.com>
2024-03-12 16:11:57 +0100

    new commit

--- index.js
+++ index.js
@@ -2,22 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./src/config/db");
 const motosRouter = require("./src/api/routes/moto");
-
-
-
-
+const PORT = 3000
 const app = express();
 connectDB();
 app.use(express.json());
 
 app.use("/api/v1/motos", motosRouter);
 
-
-
 app.use("*", (req, res, next) => {
     return res.status(404).json("Route Not Founds");
 });
 
-app.listen(3000, () => {
-    console.log("http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
 })
